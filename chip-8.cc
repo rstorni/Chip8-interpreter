d8fa8216f3a4d4eee986f0f597b8e5689cd77afc
Ramiro <rstorni@bowdoin.edu>
2022-12-24 00:48:06 -0500

    adding chip8  source code

---
+++ chip-8.cc
@@ -0,0 +1,282 @@
+#include "chip-8.h"
+#include <chrono>
+#include <random>
+#include <fstream>
+
+const unsigned int FONT_SET_SIZE = 80;
+const unsigned int FONT_START_ADDRESS = 0x050;
+const unsigned int ROM_START_ADDRESS = 0x200;
+
+uint8_t fontSet[FONT_SET_SIZE] =  
+	{
+		0xF0, 0x90, 0x90, 0x90, 0xF0, // 0
+		0x20, 0x60, 0x20, 0x20, 0x70, // 1
+		0xF0, 0x10, 0xF0, 0x80, 0xF0, // 2
+		0xF0, 0x10, 0xF0, 0x10, 0xF0, // 3
+		0x90, 0x90, 0xF0, 0x10, 0x10, // 4
+		0xF0, 0x80, 0xF0, 0x10, 0xF0, // 5
+		0xF0, 0x80, 0xF0, 0x90, 0xF0, // 6
+		0xF0, 0x10, 0x20, 0x40, 0x40, // 7
+		0xF0, 0x90, 0xF0, 0x90, 0xF0, // 8
+		0xF0, 0x90, 0xF0, 0x10, 0xF0, // 9
+		0xF0, 0x90, 0xF0, 0x90, 0x90, // A
+		0xE0, 0x90, 0xE0, 0x90, 0xE0, // B
+		0xF0, 0x80, 0x80, 0x80, 0xF0, // C
+		0xE0, 0x90, 0x90, 0x90, 0xE0, // D
+		0xF0, 0x80, 0xF0, 0x80, 0xF0, // E
+		0xF0, 0x80, 0xF0, 0x80, 0x80  // F
+	};
+
+
+//:randGen(std::chrono:system_clock::now().time_since_epoch().count())
+Chip8::Chip8()
+{
+	// sets the pc to the starting address location
+	pc = ROM_START_ADDRESS;
+	
+	// Loads the font set into the RAM
+	for(unsigned int i = 0; i < FONT_SET_SIZE; i++)
+	{
+		memory[FONT_START_ADDRESS + i] = fontSet[i];
+	}
+
+	
+	
+}
+
+//loads binary file data into the correct spot in memory
+void Chip8::loadROM(char const* filename)
+{
+	//creates an ifstream object tied to the given filename
+	//also specifies that the file is binary
+	std::ifstream rom_file(filename, std::ios_base::binary);
+		
+	if(rom_file.is_open())
+	{
+		//gets the length of the file and reads in data from file to a buffer;
+		rom_file.seekg(0, rom_file.end);
+		int length = rom_file.tellg();
+		rom_file.seekg(0, rom_file.beg);
+		char* buffer = new char[length];
+		rom_file.read(buffer, length);
+		rom_file.close();
+			
+		for(long i = 0; i < length; i++)
+		{
+			Chip8::memory[ROM_START_ADDRESS + i] = buffer[i];
+		}
+			
+		delete[] buffer;
+	}
+}
+
+//Clear Screen
+void Chip8::op_00E0()
+{
+	//TO DO
+}
+
+//RETURN
+void Chip8::op_00EE()
+{
+	--stack_pointer;
+	pc = stack[stack_pointer];
+}
+
+//JUMP
+void Chip8::op_1nnn()
+{
+	pc = opcodes & 0x0FFFU; 
+}
+
+//CALL
+void Chip8::op_2nnn()
+{	
+	uint16_t address = opcodes & 0x0FFFU;
+	stack[stack_pointer] = pc;
+	++stack_pointer;
+	pc = address;
+	
+}
+
+void Chip8::op_3xkk()
+{
+	uint8_t compared_value = opcodes & 0x00FFU;
+	uint8_t Vx = (opcodes & 0x0F00U)>> 8U;
+	
+	if(compared_value == regesters[Vx])
+	{
+		pc += 2;
+	}	
+}
+
+void Chip8::op_4xkk()
+{
+	uint8_t compared_value = opcodes & 0x00FFU;
+	uint8_t Vx = (opcodes & 0x0F00U) >> 8U;
+	
+	if(compared_value != regesters[Vx])
+	{
+		pc += 2;
+	}
+}
+
+void Chip8::op_5xy0()
+{
+	uint8_t Vy = (opcodes & 0x00F0) >> 4U;
+	uint8_t Vx = (opcodes & 0x0F00) >> 8U;
+	
+	if(regesters[Vx] == regesters[Vy])
+	{
+		pc += 2;
+	}
+}
+
+void Chip8::op_6xkk()
+{
+	uint8_t load_value = opcodes & 0x00FFU;
+	uint8_t Vx = (opcodes & 0x0F00U) >> 8U;
+	regesters[Vx] = load_value;
+}
+
+void Chip8::op_7xkk()
+{
+	uint8_t byte = opcodes & 0x00FFU;
+	uint8_t Vx = (opcodes & 0x0F00U) >> 8U;
+	regesters[Vx] += byte;
+}
+
+void Chip8::op_8xy0()
+{
+	uint8_t Vy = (opcodes & 0x00F0U) >> 4U;
+	uint8_t Vx = (opcodes & 0x0F00U) >> 8U;
+	regesters[Vx] = regesters[Vy];
+}
+
+void Chip8::op_8xy1()
+{
+	uint8_t Vy = (opcodes & 0x00F0U) >> 4U;
+	uint8_t Vx = (opcodes & 0x0F00U) >> 8U;
+	
+	regesters[Vx] = (regesters[Vx] | regesters[Vy]);
+}
+
+void Chip8::op_8xy2()
+{
+	uint8_t Vy = (opcodes & 0x00F0U) >> 4U;
+	uint8_t Vx = (opcodes & 0x0F00U) >> 8U;
+	
+	regesters[Vx] = (regesters[Vx] & regesters[Vy]);
+}
+
+void Chip8::op_8xy3()
+{
+	uint8_t Vy = (opcodes & 0x00F0U) >> 4U;
+	uint8_t Vx = (opcodes & 0x0F00U) >> 8U;
+	regesters[Vx] = (regesters[Vx] ^ regesters[Vy]);
+}
+
+void Chip8::op_8xy4()
+{
+	//TO DO
+}
+
+void Chip8::op_8xy5()
+{
+	//TO DO
+}
+
+void Chip8::op_8xy6()
+{
+	//TO DO
+}
+
+void Chip8::op_8xy7()
+{
+	//TO DO
+}
+
+void Chip8::op_8xyE()
+{
+	//TO DO
+}
+
+void Chip8::op_9xy0()
+{
+	//TO DO
+}
+
+void Chip8::op_Annn()
+{
+	//TO DO
+}
+
+void Chip8::op_Bnnn()
+{
+	//TO DO
+}
+
+void Chip8::op_Cxkk()
+{
+	//To DO
+}
+
+void Chip8::op_Dxyn()
+{
+	//TO DO
+}
+
+void Chip8::op_Ex9E()
+{
+	//TO DO
+}
+
+void Chip8::op_ExA1()
+{
+	//TO DO
+}
+
+void Chip8::op_Fx07()
+{
+	//To DO
+}
+
+void Chip8::op_Fx0A()
+{
+	//To DO
+}
+
+void Chip8::op_Fx15()
+{
+	//To DO
+}
+
+void Chip8::op_Fx18()
+{
+	//To DO
+}
+
+void Chip8::op_Fx1E()
+{
+	//To DO
+}
+
+void Chip8::op_Fx29()
+{
+	//To DO
+}
+
+void Chip8::op_Fx33()
+{
+	//TO DO 
+}
+
+void Chip8::op_Fx55()
+{
+	//To DO
+}
+
+void Chip8::op_Fx65()
+{
+	//To DO
+}
---
+++ chip-8.h
@@ -0,0 +1,149 @@
+#ifndef CHIP8_H
+#define CHIP8_H
+#include <cstdint>
+
+//CONSTANTS
+const unsigned int MEMORY_SIZE = 4096;
+const unsigned int NUM_REGESTERS = 16;
+const unsigned int STACK_SIZE = 16;
+const unsigned int NUM_KEYS = 16;
+const unsigned int DISPLAY_HIGHT = 32;
+const unsigned int DISPLAY_WIDTH = 64;
+
+class Chip8{
+public:
+	
+	Chip8(); //constructor
+	void loadROM(char const* filename);
+	void cycle();
+	
+	uint8_t keypad[NUM_KEYS];
+	uint32_t display[DISPLAY_HIGHT * DISPLAY_WIDTH];
+
+private:
+
+	//34 chip8 instructions
+	
+	//Does Nothing
+	void op_null();
+	
+	//jump to machine code routine at nnn (obsolite)
+	void op_0nnn();
+	
+	//CLS clears the display
+	void op_00E0();	
+
+	//RET returns from a subroutine
+	void op_00EE();
+	
+	//JP jump to location nnn
+	void op_1nnn();
+	
+	//CALL call subroutine at nnn
+	void op_2nnn();
+
+	//SE Vx skip the next instruction if Vx == kk
+	void op_3xkk();
+
+	//SNE Vx skip next instruction if Vx != kk
+	void op_4xkk();
+
+	//SE Vx, Vy skip the next instruction if Vx == Vy
+	void op_5xy0();
+
+	//LD Vx load the value kk into regester Vx
+	void op_6xkk();
+	
+	//ADD Vx adds the value of kk to the val at regester Vx and then stores it at Vx
+	void op_7xkk(); 
+
+	//LD Vx, Vy Load value of regester Vy into regester Vx
+	void op_8xy0();
+	
+	//OR Vx, Vy Set Vx equal to the result from preforming OR on Vx Vy (Vx | Vy)
+	void op_8xy1();
+	
+	//AND Vx, Vy set Vx equal to the result from preforming AND on Vx Vy (Vx & Vy)
+	void op_8xy2();
+
+	//XOR Vx, Vy set Vx equal to the result from preforming XOR on Vx Vy (Vx ^ Vy)
+	void op_8xy3();
+
+	//ADD Vx, Vy set Vx equal to the sum of Vx and Vy also set the VF regester to carry
+	void op_8xy4();
+
+	//SUB Vx, Vy set Vx equal to the difference of Vx and Vy set VF to NOT borrow
+	void op_8xy5();
+	
+	//SHR Vx {, Vy} set Vx to Vx SHR 1
+	void op_8xy6();
+
+	//SUBN Vx, Vy set Vx equal to the difference of Vy and Vx set VF to NOT borrow
+	void op_8xy7();
+	
+	//SHL Vx {, Vy} set Vx equal to Vx SHL 1
+	void op_8xyE();
+	
+	//SNE Vx,Vy Skip next instruction if Vx != Vy
+	void op_9xy0();
+
+	//LD I, addr Load the value nnn into regester I
+	void op_Annn();
+	
+	//JP V0, addr Jump to the location of nnn + V0
+	void op_Bnnn();
+
+	//RND Vx, set Vx to the result from a random byte AND kk
+	void op_Cxkk();
+
+	//DRW Vx, Vy display n-byte sprite starting at memory location I at (Vx,Vy),Set VF = Collison
+	void op_Dxyn();
+	
+	//SKP Vx Skip the next instruction if the key with value of Vx is pressed
+	void op_Ex9E();
+
+	//SKNP Vx Skip the next instruction if the key with value Vx is not pressed
+	void op_ExA1();
+
+	//LD Vx, DT Set Vx to the value of the delay timer
+	void op_Fx07();
+
+	//LD Vx, K Wait for a key to be pressed and store the value of the key in Vx
+	void op_Fx0A();
+
+	//LD DT,Vx Set the delay timer equal to Vx
+	void op_Fx15();
+
+	//LD St, Vx Set the sound timer to equal Vx
+	void op_Fx18();
+
+	//ADD I, Vx Set I equal to the sum of I and Vx
+	void op_Fx1E();
+	
+	//LD F, Vx Set I equal to the location of sprite for digit Vx
+	void op_Fx29();
+	
+	//Ld B, Vx Store BCD reprsentation of Vx in Memory locations I, I+1 and I+2
+	void op_Fx33();
+
+	//LD [I], Vx Store regesters V0 - Vx in memory starting at location I
+	void op_Fx55();
+	
+	//LD Vx, [I] Read regesters V0 - Vx from memory starting at location I
+	void op_Fx65();
+
+
+	uint8_t memory[MEMORY_SIZE];
+	uint8_t regesters[NUM_REGESTERS];	
+	uint16_t pc;
+	uint16_t index_regester;
+	uint16_t stack[STACK_SIZE];
+	uint8_t stack_pointer;
+	uint8_t sound_timer;
+	uint8_t delay_timer;
+	uint16_t opcodes;
+
+};
+
+
+#endif
