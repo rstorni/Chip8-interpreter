ededb884667b59e9bcc5741c94e42d0deac4dd1e
Ramiro <rstorni@bowdoin.edu>
2022-12-31 20:07:44 -0500

    incorporated the keyboard

--- gameWindow.cc
+++ gameWindow.cc
@@ -1,4 +1,6 @@
 #include "gameWindow.h"
+#include "chip-8.cc"
+
 
 GameWindow::GameWindow(char const* title, int windowWidth, int windowHeight, int texturedWidth, int texturedHeight)
 {
@@ -63,5 +65,109 @@ void GameWindow::Update(void const* buffer, int pitch)
 
 bool GameWindow::ProcessInput(uint8_t* keys)
 {
+	bool quit = false;
+	SDL_Event event;
+	
+	while(SDL_PollEvent(&event))
+	{
+		switch(event.type)
+		{
+			case SDL_QUIT:
+			{
+				quit = true;
+			}break;
+
+			case SDL_KEYDOWN:
+			{
+				switch(event.key.keysym.sym)
+				{
+					case SDLK_ESCAPE:
+					{
+						quit = true;
+					}break;
+		
+					case SDLK_x:
+					{
+						keys[0] = 1;	
+					}break;
+
+					case SDLK_1:
+					{
+						keys[1] = 1;
+					}break;
+
+					case SDLK_2:
+					{
+						keys[2] = 1;
+					}break;
+				
+					case SDLK_3:
+					{
+						keys[3] = 1;
+					}break;
+				
+					case SDLK_q:
+					{
+						keys[4] = 1
+					}break;
+					
+					case SDLK_w:
+					{
+						keys[5] = 1;
+					}break;
+	
+					case SDLK_e:
+					{
+						keys[6] = 1;
+					}break;
+	
+					case SDLK_a:
+					{
+						keys[7] = 1;
+					}break;
+
+					case SDLK_s:
+					{
+						keys[8] = 1;
+					}break;
+
+					case SDLK_d:
+					{
+						keys[9] = 1;
+					}break;
+					
+					case SDLK_z:
+					{
+						keys[0xA] = 1;
+					}break;
+					
+					case SDLK_c:
+					{
+						keys[0xB] = 1;
+					}break;
+					
+					case SDLK_4:
+					{
+						keys[0xC] = 1;
+					}break;
+					
+					case SDLK_r:
+					{
+						keys[0xD] = 1;
+					}break;
+					
+					case SDLK_f:
+					{
+						keys[0xE] = 1;
+					}break;
+
+					case SDLK_v:
+					{
+						keys[0xF] = 1;
+					}break;				
+				}
+			}
+		}
+	}
 	
 }
